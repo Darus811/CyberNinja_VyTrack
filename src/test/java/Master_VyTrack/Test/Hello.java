fed1f1120d7f51cf6828fee2d5afb080fb069102
GeryaM <geryam112@gmail.com>
2021-06-13 18:32:29 -0400

    Printed "Hello Word"

---
+++ src/test/java/Master_VyTrack/Test/Hello.java
@@ -0,0 +1,10 @@
+package Master_VyTrack.Test;
+
+public class Hello {
+    //I will now main Method
+    public static void main(String[] args) {
+
+        System.out.println("Hello Word");
+    }
+
+}
